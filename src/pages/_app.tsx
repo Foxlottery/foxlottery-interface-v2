a2d90c96fab67bbf5fe7c0deedd536a4fc26bc23
0xElliotAlderson <>
2022-08-19 20:56:55 +0900

    fix: Change meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -67,17 +67,17 @@ function MyApp(props: AppProps) {
         />
         <meta
           name="description"
-          content="Cheaper and faster than Uniswap? Discover PancakeSwap, the leading DEX on BNB Smart Chain (BSC) with the best farms in DeFi and a lottery for CAKE."
+          content="GamblingDao offers gambling on a decentralized blockchain network.We maintain low costs and donate the profits earned to better society."
         />
         <meta name="theme-color" content="#1FC7D4" />
         <meta name="twitter:image" content="https://gamblingdao.org/images/hero.png" />
         <meta
           name="twitter:description"
-          content="The most popular AMM on BSC! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by PancakeSwap), NFTs, and more, on a platform you can trust."
+          content="GamblingDao offers gambling on a decentralized blockchain network.We maintain low costs and donate the profits earned to better society."
         />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="🥞 PancakeSwap - A next evolution DeFi exchange on BNB Smart Chain (BSC)" />
-        <title>PancakeSwap</title>
+        <meta name="twitter:title" content="GamblingDao" />
+        <title>GamblingDao</title>
         {(Component as NextPageWithLayout).mp && (
           // eslint-disable-next-line @next/next/no-sync-scripts
           <script src="https://public.bnbstatic.com/static/js/mp-webview-sdk/webview-v1.0.0.min.js" id="mp-webview" />
