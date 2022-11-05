438335b855753efa01eec1e5b1722a522f86b88e
PeterTakahashi <>
2022-11-05 20:30:33 +0900

    fix window interface

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,3 +1,17 @@
 type SerializedBigNumber = string
 
 declare let __NEZHA_BRIDGE__: any
+
+interface Window {
+  ethereum?: {
+    isMetaMask?: true
+    isOpera?: true
+    isCoinbaseWallet?: true
+    isTrust?: true
+    providers?: any[]
+    request?: (...args: any[]) => Promise<void>
+  }
+  BinanceChain?: {
+    bnbSign?: (address: string, message: string) => Promise<{ publicKey: string; signature: string }>
+  }
+}
