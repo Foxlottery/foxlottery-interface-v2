2ff756f0284dd630ea73a17bb7cb4e89cb2721ef
PeterTakahashi <seiya.takahashi@rights-s.jp>
2022-11-06 15:43:46 +0900

    change modal

--- src/components/Menu/GlobalSettings/SettingsModal.tsx
+++ src/components/Menu/GlobalSettings/SettingsModal.tsx
@@ -76,34 +76,10 @@ const SettingsModal: React.FC<InjectedModalProps> = ({ onDismiss, mode }) => {
         {mode === SettingsMode.GLOBAL && (
           <>
             <Flex pb="24px" flexDirection="column">
-              <Text bold textTransform="uppercase" fontSize="18px" color="secondary" mb="24px">
-                {t('Global')}
-              </Text>
               <Flex justifyContent="space-between" mb="24px">
                 <Text>{t('Dark mode')}</Text>
                 <ThemeSwitcher isDark={isDark} toggleTheme={() => setTheme(isDark ? 'light' : 'dark')} />
               </Flex>
-              <Flex justifyContent="space-between" alignItems="center" mb="24px">
-                <Flex alignItems="center">
-                  <Text>{t('Subgraph Health Indicator')}</Text>
-                  <QuestionHelper
-                    text={t(
-                      'Turn on NFT market subgraph health indicator all the time. Default is to show the indicator only when the network is delayed',
-                    )}
-                    placement="top-start"
-                    ml="4px"
-                  />
-                </Flex>
-                <Toggle
-                  id="toggle-subgraph-health-button"
-                  checked={subgraphHealth}
-                  scale="md"
-                  onChange={() => {
-                    setSubgraphHealth(!subgraphHealth)
-                  }}
-                />
-              </Flex>
-              <GasSettings />
             </Flex>
           </>
         )}
